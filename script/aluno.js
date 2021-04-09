f4872a9a6b00142a19708084161fef9da735d14d
lucasmetron <lucasmetron@gmail.com>
2021-04-09 12:09:32 -0300

    animações nos butoes com troca de img

---
+++ script/aluno.js
@@ -0,0 +1,9 @@
+let mudaImg = (id,img) =>{
+    let image = document.getElementById(id).children[0]
+    image.setAttribute('src',img);
+}
+
+let voltaImg = (id,img) =>{
+    let image = document.getElementById(id).children[0]
+    image.setAttribute('src',img)
+}
