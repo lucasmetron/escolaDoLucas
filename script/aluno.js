b7cb172e594ce30a0b90bd335e4e6c25b758f2ae
lucasmetron <lucasmetron@gmail.com>
2021-04-09 14:20:22 -0300

    colocado engrenagem para alterar foto

--- script/aluno.js
+++ script/aluno.js
@@ -1,9 +1,11 @@
 let mudaImg = (id,img) =>{
     let image = document.getElementById(id).children[0]
     image.setAttribute('src',img);
+    // console.log(image)
 }
 
 let voltaImg = (id,img) =>{
     let image = document.getElementById(id).children[0]
     image.setAttribute('src',img)
+    // console.log(image)
 }
