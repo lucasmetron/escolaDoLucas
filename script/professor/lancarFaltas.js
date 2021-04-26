171e7ce428b4bc54ef1aace792d9e441009df951
lucasmetron <lucasmetron@gmail.com>
2021-04-26 14:18:44 -0300

    inserido no tança notas e faltas o verificador CVa

--- script/professor/lancarFaltas.js
+++ script/professor/lancarFaltas.js
@@ -1,7 +1,7 @@
-async function listaAluno() {
-    let turmaSelecionada = document.querySelector("#turmaExcluirAlunoInput").value
+async function listaAlunoFaltas() {
+    let turmaSelecionada = document.querySelector("#anoTurmaFaltas").value
 
-    let tagSelect = document.querySelector("#NomeExcluirAlunoInput");
+    let tagSelect = document.querySelector("#nomeAlunoFaltas");
     tagSelect.innerHTML = '<option value="">Selecione o Aluno</option>';
 
     await alert(`Atenção! Caso o campo nome não apresente listagem, a turma ${turmaSelecionada} não existe.`)
@@ -17,13 +17,76 @@ async function listaAluno() {
             let infoAluno = nomeAluno + "/ Matricula:" + matriculaAluno;
             let info = document.createTextNode(infoAluno)
 
-            let tagSelect = document.querySelector('#NomeExcluirAlunoInput')
+
             novaTagOption.append(info);
             novaTagOption.setAttribute('value', matriculaAluno)
             tagSelect.appendChild(novaTagOption);
-
-            console.log(info)
         })
     })
+}
 
+function verificaCamposFaltas() {
+
+
+    if (document.querySelector("#anoTurmaFaltas").value == '') {
+        alert("Preencha todos os campos! Campo 'Ano/Turma' vazio.")
+        document.querySelector("#anoTurmaFaltas").focus()
+    }
+
+    else if (document.querySelector("#nomeAlunoFaltas").value == '') {
+        alert("Preencha todos os campos! Campo 'Aluno' vazio.");
+        document.querySelector("#nomeAlunoFaltas").focus();
+
+    }
+
+    else if (document.querySelector("#disciplinaFaltas").value == '') {
+        alert("Preencha todos os campos! Campo 'Disciplina' vazio.")
+        document.querySelector("#disciplinaFaltas").focus()
+    }
+
+    else if (document.querySelector("#bimestreFaltas").value == '') {
+        alert("Preencha todos os campos! Campo 'Bimestre'vazio.")
+        document.querySelector("#bimestreFaltas").focus()
+    }
+
+    else if (document.querySelector("#faltaLancada").value == '') {
+        alert("Preencha todos os campos! Campo 'Nota' vazio.")
+        document.querySelector("#faltaLancada").focus()
+
+    } else {
+        console.log('cheguei no else')
+        lancaFalta();
+    }
+
+
+
+}
+
+async function lancaFalta() {
+    let anoTurma = document.querySelector("#anoTurmaFaltas").value;
+    let matricula = document.querySelector("#nomeAlunoFaltas").value;
+    let disciplina = document.querySelector("#disciplinaFaltas").value;
+    let bimestre = document.querySelector("#bimestreFaltas").value;
+    let falta = document.querySelector("#faltaLancada").value;
+
+    console.log(anoTurma, matricula, disciplina, bimestre, falta)
+
+    await db.collection(anoTurma).doc(matricula).set(
+        {
+            [disciplina]: {
+                [bimestre]: {
+                    faltas: falta
+                }
+            }
+        }, { merge: true }
+
+    ).catch(error => {
+        alert(error)
+    })
+
+    alert(`Faltas lançada com sucesso`);
+
+    setTimeout(() => {
+        window.location.reload();
+    }, 1000)
 }
--- script/professor/lancarNotas.js
+++ script/professor/lancarNotas.js
@@ -28,11 +28,49 @@ async function listaAlunoNotas() {
 
 }
 
+function verificaCamposNotas() {
+
+
+    if (document.querySelector("#anoTurmaNotas").value == '') {
+        alert("Preencha todos os campos! Campo 'Ano/Turma' vazio.")
+        document.querySelector("#anoTurmaNotas").focus()
+    }
+
+    else if (document.querySelector("#nomeAlunoNota").value == '') {
+
+        alert("Preencha todos os campos! Campo 'Aluno' vazio.");
+        document.querySelector("#nomeAlunoNota").focus();
+
+    }
+
+    else if (document.querySelector("#disciplinaNota").value == '') {
+        alert("Preencha todos os campos! Campo 'Disciplina' vazio.")
+        document.querySelector("#disciplinaNota").focus()
+    }
+
+    else if (document.querySelector("#bimestreNota").value == '') {
+        alert("Preencha todos os campos! Campo 'Bimestre'vazio.")
+        document.querySelector("#bimestreNota").focus()
+    }
+
+    else if (document.querySelector("#notaLancada").value == '') {
+        alert("Preencha todos os campos! Campo 'Nota' vazio.")
+        document.querySelector("#notaLancada").focus()
+
+    } else {
+        console.log('cheguei no else')
+        lancaNota();
+    }
+
+
+
+}
+
 async function lancaNota() {
     let anoTurma = document.querySelector("#anoTurmaNotas").value;
     let matricula = document.querySelector("#nomeAlunoNota").value;
-    let disciplina = document.querySelector("#disciplinaAluno").value;
-    let bimestre = document.querySelector("#bimestre").value;
+    let disciplina = document.querySelector("#disciplinaNota").value;
+    let bimestre = document.querySelector("#bimestreNota").value;
     let nota = document.querySelector("#notaLancada").value;
 
     console.log(anoTurma, matricula, disciplina, bimestre, nota)
