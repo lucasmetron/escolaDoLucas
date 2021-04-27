3b91ada2084cb622b9527e721c1b424022bf0eda
lucasmetron <lucasmetron@gmail.com>
2021-04-27 14:06:46 -0300

    upload material feito ajustando tela cadastro list

--- script/professor/uploadMaterial.js
+++ script/professor/uploadMaterial.js
@@ -1,32 +1,63 @@
 let arq = "arquivo"
 let storage = firebase.storage();
 
+function verificaCamposMaterial() {
+
+    if (document.querySelector("#nomeAtiviProfessor").value == '') {
+        alert("Preencha todos os campos! Campo 'Nome da atividade' vazio.")
+        document.querySelector("#nomeAtiviProfessor").focus()
+    }
+
+    else if (document.querySelector("#disciplinaMAterialProfessor").value == '') {
+
+        alert("Preencha todos os campos! Campo 'Disciplina' vazio.");
+        document.querySelector("#disciplinaMAterialProfessor").focus();
+
+    }
+
+    else if (document.querySelector("#materialProfessor").files[0] == '' || undefined || null) {
+        alert("Preencha todos os campos! Campo 'Arquivo' vazio.")
+        document.querySelector("#materialProfessor").focus()
+
+        // função verificar se há outro valor igual
+    }
+
+    else {
+        subirMaterial();
+    }
+
+}
 
 async function subirMaterial() {
     let nomeAtividade = document.querySelector("#nomeAtiviProfessor").value;
     let disciplina = document.querySelector("#disciplinaMAterialProfessor").value;
     let arquivo = document.querySelector("#materialProfessor").files[0]
     let storageRef = firebase.storage().ref();
-    let documentoRef = storageRef.child(arquivo.type + "/" + arquivo.name)
+    let documentoRef = storageRef.child("material/" + arquivo.name)
 
-    // db.collection(arq).doc(nomeAtividade).set({
-    //     nomeAtividade: nomeAtividade,
-    //     disciplina: disciplina,
-    //     arquivo: arquivo
-    // }).then(doc => {
-    //     alert("Atividade salva na nuvem.")
-    // }).catch(erro => {
-    //     alert(erro)
-    // })
+    alert("Seu material será salvo!");
 
-    alert("Carregando, por favor aguarde...")
+    await db.collection(arq).doc(nomeAtividade).set({
+        nomeAtividade: nomeAtividade,
+        disciplina: disciplina,
+        nomeArquivo: arquivo.name
+    }).then(doc => {
+        console.log("Atividade salva na nuvem.")
+    }).catch(erro => {
+        alert(erro)
+    })
 
-    documentoRef.put(arquivo).then((snapshot) => {
+    alert("Subindo arquivo para nuvem, por favor aguarde...");
+
+    await documentoRef.put(arquivo).then((snapshot) => {
         alert("Upload feito!")
     }).catch(erro => {
         alert(erro)
     })
 
-
+    setTimeout(() => {
+        window.location.reload();
+    }, 500)
 }
+
 
