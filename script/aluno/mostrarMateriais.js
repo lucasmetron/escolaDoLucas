8ce6e33eb399cf025784d7b7bb0cf1fa2c324c5d
lucasmetron <lucasmetron@gmail.com>
2021-05-12 10:09:22 -0300

    funação baixar material concluida

--- script/aluno/mostrarMateriais.js
+++ script/aluno/mostrarMateriais.js
@@ -1,6 +1,7 @@
 let materialLocal = "arquivo"
 let materiais = [];
 
+const storage = firebase.storage();
 
 db.collection(materialLocal).onSnapshot(snapshot => {
     let tela = document.querySelector("#mostradorMaterial");
@@ -22,43 +23,91 @@ db.collection(materialLocal).onSnapshot(snapshot => {
         })
 
 
+
+
         materiais.forEach(item => {
 
-            let materialArea = document.querySelector("#mostradorMaterial")
 
-            let tagAtividade = document.createElement("div");
-            let tagMateria = document.createElement("div");
-            let tagProfe = document.createElement("div");
-            let tagLink = document.createElement("a");
+            let ref = storage.ref(`/material/${item.nomeArquivo}`);
+            let URL;
 
-            let infoAtividade = document.createTextNode(item.nomeAtividade)
-            let infoMateria = document.createTextNode(item.disciplina)
-            let infoProfe = document.createTextNode(item.nomeProfessor)
-            let infoLink = document.createTextNode("Acesse aqui")
+            ref.getDownloadURL().then(url => {
+                URL = url;
+            })
 
+            setTimeout(() => { //essa espera é para esperar o arquivo do download carregar
+                let materialArea = document.querySelector("#mostradorMaterial")
 
-            tagAtividade.append(infoAtividade)
-            tagAtividade.setAttribute("id", "tituloMaterial")
-            tagAtividade.setAttribute("class", "campo")
+                let tagAtividade = document.createElement("div");
+                let tagMateria = document.createElement("div");
+                let tagProfe = document.createElement("div");
+                let tagLink = document.createElement("a");
 
-            tagMateria.append(infoMateria)
-            tagMateria.setAttribute("id", "materiaMaterial")
-            tagMateria.setAttribute("class", "campo")
-
-            tagProfe.append(infoProfe)
-            tagProfe.setAttribute("id", "professorMaterial")
-            tagProfe.setAttribute("class", "campo")
-
-            tagLink.append(infoLink)
-            tagLink.href = "https://docs.google.com/uc?export=download&id=1zw2gHRk6MnQz3d-n194eqGKmTb1w-Q5J"
-            tagLink.setAttribute("class", "campo amarelo link ")
-            tagLink.target = "_blanck"
+                let infoAtividade = document.createTextNode(item.nomeAtividade)
+                let infoMateria = document.createTextNode(item.disciplina)
+                let infoProfe = document.createTextNode(item.nomeProfessor)
+                let infoLink = document.createTextNode("Acesse aqui")
 
 
-            materialArea.append(tagAtividade)
-            materialArea.append(tagMateria)
-            materialArea.append(tagProfe)
-            materialArea.append(tagLink)
+                tagAtividade.append(infoAtividade)
+                tagAtividade.setAttribute("id", "tituloMaterial")
+                tagAtividade.setAttribute("class", "campo")
+
+                tagMateria.append(infoMateria)
+                tagMateria.setAttribute("id", "materiaMaterial")
+                tagMateria.setAttribute("class", "campo")
+
+                tagProfe.append(infoProfe)
+                tagProfe.setAttribute("id", "professorMaterial")
+                tagProfe.setAttribute("class", "campo")
+
+                tagLink.append(infoLink)
+                tagLink.href = URL;
+                tagLink.setAttribute("class", "campo amarelo link ")
+                tagLink.target = "_blanck"
+
+
+                materialArea.append(tagAtividade)
+                materialArea.append(tagMateria)
+                materialArea.append(tagProfe)
+                materialArea.append(tagLink)
+            }, 1000)
+
+            // let materialArea = document.querySelector("#mostradorMaterial")
+
+            // let tagAtividade = document.createElement("div");
+            // let tagMateria = document.createElement("div");
+            // let tagProfe = document.createElement("div");
+            // let tagLink = document.createElement("a");
+
+            // let infoAtividade = document.createTextNode(item.nomeAtividade)
+            // let infoMateria = document.createTextNode(item.disciplina)
+            // let infoProfe = document.createTextNode(item.nomeProfessor)
+            // let infoLink = document.createTextNode("Acesse aqui")
+
+
+            // tagAtividade.append(infoAtividade)
+            // tagAtividade.setAttribute("id", "tituloMaterial")
+            // tagAtividade.setAttribute("class", "campo")
+
+            // tagMateria.append(infoMateria)
+            // tagMateria.setAttribute("id", "materiaMaterial")
+            // tagMateria.setAttribute("class", "campo")
+
+            // tagProfe.append(infoProfe)
+            // tagProfe.setAttribute("id", "professorMaterial")
+            // tagProfe.setAttribute("class", "campo")
+
+            // tagLink.append(infoLink)
+            // tagLink.href = URL;
+            // tagLink.setAttribute("class", "campo amarelo link ")
+            // tagLink.target = "_blanck"
+
+
+            // materialArea.append(tagAtividade)
+            // materialArea.append(tagMateria)
+            // materialArea.append(tagProfe)
+            // materialArea.append(tagLink)
 
 
         })
@@ -70,37 +119,17 @@ db.collection(materialLocal).onSnapshot(snapshot => {
 })
 
 
-function baixaArquivo() {
-    // Create a reference with an initial file path and name
-    var storage = firebase.storage();
-    var pathReference = storage.ref('arquivo.pdf');
+async function baixaArquivo(arquivo) {
 
-    // Create a reference from a Google Cloud Storage URI
-    var gsReference = storage.refFromURL('gs://escoladolucas-81f7c.appspot.com/material/19-GUERRA-FRIA-2019-LISTA.pdf.googleapis.com/b/bucket/o/images%20stars.jpg')
+    let ref = storage.ref(`/material/${arquivo}`);
+    let URL;
 
-    // Create a reference from an HTTPS URL
-    // Note that in the URL, characters are URL escaped!
-    var httpsReference = storage.refFromURL('https://gs://escoladolucas-81f7c.appspot.com/material/19-GUERRA-FRIA-2019-LISTA.pdf.googleapis.com/b/bucket/o/images%20stars.jpg');
+    await ref.getDownloadURL().then(url => {
+        URL = url;
+    })
 
-    storageRef.child('gs://escoladolucas-81f7c.appspot.com/').getDownloadURL().then(function (url) {
-        // `url` is the download URL for 'images/stars.jpg'
-
-        // This can be downloaded directly:
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = 'blob';
-        xhr.onload = function (event) {
-            var blob = xhr.response;
-        };
-        xhr.open('GET', url);
-        xhr.send();
-
-        // // Or inserted into an <img> element:
-        // var img = document.getElementById('myimg');
-        // img.src = url;
-    }).catch(function (error) {
-        // Handle any errors
-    });
-
-
+    console.log(URL)
+    return URL;
 }
+
 
